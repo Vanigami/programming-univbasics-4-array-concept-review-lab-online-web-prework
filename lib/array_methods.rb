e975fb790d2747288447021ec0aaca937c42c854
Savanna Winland <vgwinland@gmail.com>
2019-08-31 02:51:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,11 @@
 def find_element_index(array, value_to_find)
-
 array.index { |value_to_find|
   puts array[value_to_find]
 }
 end
 
 def find_max_value(array)
-  # Add your solution here
+
 end
 
 def find_min_value(array)
